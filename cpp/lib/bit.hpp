9c21176f9db06020c415574274779894fd1674fa
ryofjt <ryofjt12@gmail.com>
2024-03-24 16:32:49 +0900

    CntLzSt 追加

--- cpp/lib/bit.hpp
+++ cpp/lib/bit.hpp
@@ -1,6 +1,6 @@
 #include "base.hpp"
 
-template <typename T, int N, int& n>
+template <typename T, int N>
 class Bit {
  public:
   T bit[N + 1];
@@ -8,18 +8,18 @@ class Bit {
  public:
   Bit() { init(); }
 
-  void init() { fill_n(bit, n + 1, T()); }
+  void init() { fill_n(bit, N + 1, T()); }
   template <typename It>
   void init(It first) {
-    vector<T> x(n + 1);
-    copy(first, first + n, x.begin() + 1);
-    rep(i, n) { x[i + 1] += x[i]; }
-    rep(i, n + 1) { bit[i] = x[i] - x[i - (i & -i)]; }
+    vector<T> x(N + 1);
+    copy(first, first + N, x.begin() + 1);
+    rep(i, N) { x[i + 1] += x[i]; }
+    rep(i, N + 1) { bit[i] = x[i] - x[i - (i & -i)]; }
   }
 
   void add(int i, const T& x) {
     int _i = i + 1;
-    while (_i <= n) {
+    while (_i <= N) {
       bit[_i] += x;
       _i += _i & -_i;
     }
@@ -39,7 +39,7 @@ class Bit {
   // i から j - 1 までの和
   T sum(int i, int j) { return sum(j) - sum(i); }
 
-  vector<T> debug() {
+  vector<T> debug(int n) {
     vector<T> x(n);
     rep(i, n) { x[i] = sum(i, i + 1); }
     return x;
--- cpp/lib/lz_st.hpp
+++ cpp/lib/lz_st.hpp
@@ -35,15 +35,15 @@ class LzSt {
   LzSt(const T& bottom, const Comp& comp, const Op& noop, const OpComp& op_comp,
        const Apply& apply, const T& v)
       : bottom(bottom), comp(comp), noop(noop), op_comp(op_comp), apply(apply) {
-    init(v, _N);
+    init(v);
   }
 
   void init() {
     fill_n(st, _N * 2 - 1, bottom);
     fill_n(op, _N * 2 - 1, noop);
   }
-  void init(const T& v, int n) {
-    vector<T> x(n, v);
+  void init(const T& v) {
+    vector<T> x(_N, v);
     _init(x.begin(), x.end());
     fill_n(op, _N * 2 - 1, noop);
   }
@@ -115,11 +115,69 @@ class LzSt {
   template <typename It>
   T _init(It first, It last, int k = 0, int l = 0, int r = _N) {
     if (r - l == 1) {
-      return st[k] = (first + l < last ? *(first + l) : bottom);
+      return st[k] = (first + l < last ? first[l] : bottom);
     }
     return st[k] = comp(_init(first, last, k * 2 + 1, l, (l + r) / 2),
                         _init(first, last, k * 2 + 2, (l + r) / 2, r)
 
            );
+  }
+};
+
+template <typename T, typename Op, int N>
+class CntLzSt {
+ public:
+  using Comp = function<T(const T&, const T&)>;
+  using OpComp = function<Op(const Op&, const Op&)>;
+  using Apply = function<T(const Op&, const T&, int)>;
+
+ public:
+  using P = pair<T, int>;
+  using LzSt = LzSt<P, Op, N>;
+
+ public:
+  LzSt lz_st;
+
+ public:
+  inline CntLzSt(const T& bottom, const Comp& comp, const Op& noop,
+                 const OpComp& op_comp, const Apply& apply)
+      : CntLzSt(bottom, comp, noop, op_comp, apply, bottom) {}
+  inline CntLzSt(const T& bottom, const Comp& comp, const Op& noop,
+                 const OpComp& op_comp, const Apply& apply, const T& v)
+      : lz_st(
+            P(bottom, 0),
+            [=](const P& a, const P& b) {
+              return P(comp(a.first, b.first), a.second + b.second);
+            },
+            noop, op_comp,
+            [=](const Op& z, const P& a) {
+              return P(apply(z, a.first, a.second), a.second);
+            },
+            P(v, 1)) {}
+
+  inline void init() { init(lz_st.bottom.first); }
+  inline void init(const T& v) { lz_st.init(P(v, 1)); }
+  template <typename It>
+  inline void init(It first, It last) {
+    vector<P> x;
+    transform(first, last, back_inserter(x),
+              [](const T& v) { return P(v, 1); });
+    lz_st.init(x.begin(), x.end());
+  }
+
+  inline T get(int i) { return lz_st.get(i).first; }
+
+  inline void set(int i, const T& x) { return lz_st.set(i, P(x, 1)); }
+
+  inline void operate(int a, int b, const Op& z) { lz_st.operate(a, b, z); }
+
+  inline T query(int a, int b) { return lz_st.query(a, b).first; }
+
+  inline vector<T> debug(int n) {
+    auto x = lz_st.debug();
+    vector<T> y;
+    transform(x.begin(), x.end(), back_inserter(y),
+              [](const P& v) { return v.first; });
+    return y;
   }
 };
